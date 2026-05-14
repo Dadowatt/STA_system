1d3388eec51d01cbeff95590e6e2c44d0f9855cc
Dadowatt <dadowatt92@gmail.com>
2026-05-14 14:59:45 +0200

    Correction du settings.py : DEBUG, SECRET_KEY et organisation de l'environnement

--- entries/models.py
+++ entries/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Categorie(models.Model):
+    nom = models.CharField(max_length=100)
+    def __str__(self):
+        return self.nom
