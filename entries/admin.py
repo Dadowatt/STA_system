80c0462056236f80fec4da4786a37c9d27e43360
Dadowatt <dadowatt92@gmail.com>
2026-05-14 17:26:33 +0200

    Ajout des modèles Entry/Category et configuration media + admin

--- entries/admin.py
+++ entries/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
+from .models import Entry, Category
 
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nom')
 
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'auteur', 'categorie', 'date_creation')
+    search_fields = ('titre', 'contenu')
+    list_filter = ('categorie', 'date_creation')
--- entries/models.py
+++ entries/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Categorie(models.Model):
+class Category(models.Model):
     nom = models.CharField(max_length=100)
     def __str__(self):
         return self.nom
+
+
+class Entry(models.Model):
+    titre = models.CharField(max_length=100)
+    contenu = models.TextField()
+    categorie = models.ForeignKey(Category, on_delete=models.CASCADE)
+    auteur = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='entries/', blank=True, null=True)
+    date_creation = models.DateTimeField(auto_now_add=True)
